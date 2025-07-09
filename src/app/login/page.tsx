c8f12fcef4bd839fe229df5c7477b49de7e10d29
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-07-10 01:41:22 +0500

    feat: initialize Next.js application with TypeScript, Tailwind CSS, and basic structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const loginPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center mt-10">Login Page</h1>
+        </div>
+    );
+};
+
+export default loginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+const signup = ()=>{
+    return(
+        <h1 className="text-center text-2xl font-bold mt-10">
+            this is sign up page
+        </h1>
+    )
+}
+
+export default signup
